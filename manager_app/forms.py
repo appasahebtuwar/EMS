c6108f4938951d3225bf00c4df2d1d570e3723da
Appasaheb Tuwar <75714596+appasahebtuwar@users.noreply.github.com>
2020-12-22 16:12:25 +0530

    Update forms.py

--- manager_app/forms.py
+++ manager_app/forms.py
@@ -36,8 +36,7 @@ class UserRegisterForm(forms.ModelForm):
     def clean_password(self):
         password = self.cleaned_data.get('password')
         if password and len(password) < 8:
-            raise forms.ValidationError(
-                'Password must be at least 8 characters long!')
+            raise forms.ValidationError('Password must be at least 8 characters long!')
         return password
 
 # # login form
